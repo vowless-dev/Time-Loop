92610b1b3457e8bef6f4c50db4a9dd7ca7866712
VLTNOgithub <gfree6311@gmail.com>
2025-12-13 17:32:43 +0000

    backport dimension code

--- fabric/src/main/java/com/vltno/timeloop/fabric/events/PlayerFabricEvent.java
+++ fabric/src/main/java/com/vltno/timeloop/fabric/events/PlayerFabricEvent.java
@@ -1,10 +1,15 @@
 package com.vltno.timeloop.fabric.events;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import com.vltno.timeloop.events.PlayerEvent;
 
 public class PlayerFabricEvent {
     public static void afterRespawn(ServerPlayer oldPlayer, ServerPlayer newPlayer, boolean alive) {
         PlayerEvent.afterRespawn();
+    }
+
+    public static void dimensionChange(ServerPlayer player, ServerLevel origin, ServerLevel destination) {
+        PlayerEvent.dimensionChange(player);
     }
 }
