344c8f8b64df8e337d694cf29eb61231acd850e9
VLTNOgithub <gfree6311@gmail.com>
2025-12-13 17:08:50 +0000

    backport dimension code

--- common/src/main/java/com/vltno/timeloop/events/PlayerEvent.java
+++ common/src/main/java/com/vltno/timeloop/events/PlayerEvent.java
@@ -3,12 +3,19 @@ package com.vltno.timeloop.events;
 
 import com.vltno.timeloop.LoopTypes;
 import com.vltno.timeloop.TimeLoop;
+import net.minecraft.server.level.ServerPlayer;
 
 public class PlayerEvent {
     public static void afterRespawn() {
         if (TimeLoop.loopType == LoopTypes.DEATH) {
-            TimeLoop.LOOP_LOGGER.info("RESPAWNED!!!!");
+            TimeLoop.LOOP_LOGGER.info("RESPAWNED");
             TimeLoop.runLoopIteration();
         }
+    }
+
+    public static void dimensionChange(ServerPlayer player) {
+        if (!TimeLoop.isLooping) return;
+        TimeLoop.LOOP_LOGGER.info("{} CHANGED DIMENSION", player.getGameProfile().getName());
+        TimeLoop.handlePlayerDimensionChange(player);
     }
 }
