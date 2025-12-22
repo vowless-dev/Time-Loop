a3e96e8a48561a4763a56bec0d330dfb8e639c57
VLTNOgithub <gfree6311@gmail.com>
2025-12-22 16:04:40 +0000

    Move and update Types - Add TypeInterface.java

---
+++ common/src/main/java/com/vltno/timeloop/types/LoopTypes.java
@@ -0,0 +1,9 @@
+package com.vltno.timeloop.types;
+
+public enum LoopTypes implements TypeInterface {
+    TICKS,
+    TIME_OF_DAY,
+    SLEEP,
+    DEATH,
+    MANUAL;
+}
---
+++ common/src/main/java/com/vltno/timeloop/types/RewindTypes.java
@@ -0,0 +1,8 @@
+package com.vltno.timeloop.types;
+
+public enum RewindTypes implements TypeInterface {
+    NONE,
+    START_POSITION,
+    JOIN_POSITION,
+    SPAWN_POSITION;
+}
